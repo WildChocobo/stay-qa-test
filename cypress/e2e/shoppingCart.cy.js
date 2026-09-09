2e2e5a02b5276fc83f6adf9674d1e77e319174b5
WildChocobo <miki.kozus@gmail.com>
2026-09-09 02:08:02 +0200

    imports added

--- cypress/e2e/shoppingCart.cy.js
+++ cypress/e2e/shoppingCart.cy.js
@@ -1,85 +1,92 @@
-describe('Shopping cart test', () => {
-  it('should add a phone to the cart and verify the total', () => {
+import * as HomePage from '../pages/homePage';
+import * as ProductDetailPage from '../pages/productDetailPage';
+import * as NavBar from '../pages/navBar';
+import * as CartPage from '../pages/cartPage';
+import { interceptByCat, interceptViewDetail } from '../support/network';
 
-    const cartPriceColumnIndex = 2;
-    let phoneName;
-    let phonePrice;
-    let laptopName;
-    let laptopPrice;
 
-    cy.visit('https://www.demoblaze.com/index.html');
-    cy.intercept('POST', '**/bycat').as('byCat');
-    cy.contains('.list-group-item', 'Phones').click();
-    cy.wait('@byCat');
+// describe('Shopping cart test', () => {
+//   it('should add a phone to the cart and verify the total', () => {
 
-    cy.get('.card-title a').first().invoke('text').then((text) => {
-      phoneName = text.trim();
-      cy.log(phoneName);
-    });
-    cy.get('.card-block h5').first().invoke('text').then((text) => {
-      phonePrice = text.replace('$', '');
-      cy.log(phonePrice);
-    });
-    cy.get('.card-title a').first().click();
-    cy.get('#tbodyid .name').should(($el) => {
-      expect($el.text().trim()).to.eq(phoneName);
-    });
-    cy.get('#tbodyid .price-container').should(($el) => {
-      expect($el.text()).to.contain(phonePrice);
-    });
-    cy.window().then((win) => {
-      cy.stub(win, 'alert').as('alertStub');
-    });
-    cy.get('#tbodyid .btn').click();
-    cy.get('@alertStub').should('have.been.calledWith', 'Product added');
+//     const cartPriceColumnIndex = 2;
+//     let phoneName;
+//     let phonePrice;
+//     let laptopName;
+//     let laptopPrice;
 
-    cy.intercept('POST', '**/view').as('viewDetail');
-    cy.get('#cartur').click();
-    cy.wait('@viewDetail').then(() => {
-      cy.contains('#tbodyid tr', phoneName).within(() => {
-        cy.get('td').eq(cartPriceColumnIndex).should('contain.text', phonePrice);
-      });
-    });
-    cy.contains('.nav-link', 'Home').click();
-    cy.contains('.list-group-item', 'Laptops').click();
-    cy.wait('@byCat');
+//     cy.visit('https://www.demoblaze.com/index.html');
+//     cy.intercept('POST', '**/bycat').as('byCat');
+//     cy.contains('.list-group-item', 'Phones').click();
+//     cy.wait('@byCat');
 
-    cy.get('.card-title a').eq(1).invoke('text').then((text) => {
-      laptopName = text.trim();
-      cy.log(laptopName);
-    });
-     cy.get('.card-block h5').eq(1).invoke('text').then((text) => {
-      laptopPrice = text.replace('$', '');
-      cy.log(laptopPrice);
-    });
-    cy.get('.card-title a').eq(1).click();
-    cy.wait('@viewDetail');
-    cy.get('#tbodyid .name').should(($el) => {
-      expect($el.text().trim()).to.eq(laptopName);
-    });
-    cy.get('#tbodyid .price-container').should(($el) => {
-      expect($el.text()).to.contain(laptopPrice);
-    });
-    cy.window().then((win) => {
-      cy.stub(win, 'alert').as('alertStub');
-    });
-    cy.get('#tbodyid .btn').click();
-    cy.get('@alertStub').should('have.been.calledWith', 'Product added');
-    cy.get('#cartur').click();
-    cy.wait(['@viewDetail', '@viewDetail']).then(() => {
-      cy.contains('#tbodyid tr', phoneName).within(() => {
-        cy.get('td').eq(cartPriceColumnIndex).should('contain.text', phonePrice);
-      });
-      cy.get('#tbodyid').then(($el) => {
-      cy.log(`laptopName="[${laptopName}]" length=${laptopName.length}`);
-      });
-      cy.contains('#tbodyid tr', laptopName).within(() => {
-        cy.get('td').eq(cartPriceColumnIndex).should('contain.text', laptopPrice);
-      });
-       cy.get('#totalp').should(($el) => {
-        const expectedTotal = parseFloat(phonePrice) + parseFloat(laptopPrice);
-        expect(Number($el.text())).to.eq(expectedTotal);
-      });
-    });
-  });
-});
+//     cy.get('.card-title a').first().invoke('text').then((text) => {
+//       phoneName = text.trim();
+//       cy.log(phoneName);
+//     });
+//     cy.get('.card-block h5').first().invoke('text').then((text) => {
+//       phonePrice = text.replace('$', '');
+//       cy.log(phonePrice);
+//     });
+//     cy.get('.card-title a').first().click();
+//     cy.get('#tbodyid .name').should(($el) => {
+//       expect($el.text().trim()).to.eq(phoneName);
+//     });
+//     cy.get('#tbodyid .price-container').should(($el) => {
+//       expect($el.text()).to.contain(phonePrice);
+//     });
+//     cy.window().then((win) => {
+//       cy.stub(win, 'alert').as('alertStub');
+//     });
+//     cy.get('#tbodyid .btn').click();
+//     cy.get('@alertStub').should('have.been.calledWith', 'Product added');
+
+//     cy.intercept('POST', '**/view').as('viewDetail');
+//     cy.get('#cartur').click();
+//     cy.wait('@viewDetail').then(() => {
+//       cy.contains('#tbodyid tr', phoneName).within(() => {
+//         cy.get('td').eq(cartPriceColumnIndex).should('contain.text', phonePrice);
+//       });
+//     });
+//     cy.contains('.nav-link', 'Home').click();
+//     cy.contains('.list-group-item', 'Laptops').click();
+//     cy.wait('@byCat');
+
+//     cy.get('.card-title a').eq(1).invoke('text').then((text) => {
+//       laptopName = text.trim();
+//       cy.log(laptopName);
+//     });
+//      cy.get('.card-block h5').eq(1).invoke('text').then((text) => {
+//       laptopPrice = text.replace('$', '');
+//       cy.log(laptopPrice);
+//     });
+//     cy.get('.card-title a').eq(1).click();
+//     cy.wait('@viewDetail');
+//     cy.get('#tbodyid .name').should(($el) => {
+//       expect($el.text().trim()).to.eq(laptopName);
+//     });
+//     cy.get('#tbodyid .price-container').should(($el) => {
+//       expect($el.text()).to.contain(laptopPrice);
+//     });
+//     cy.window().then((win) => {
+//       cy.stub(win, 'alert').as('alertStub');
+//     });
+//     cy.get('#tbodyid .btn').click();
+//     cy.get('@alertStub').should('have.been.calledWith', 'Product added');
+//     cy.get('#cartur').click();
+//     cy.wait(['@viewDetail', '@viewDetail']).then(() => {
+//       cy.contains('#tbodyid tr', phoneName).within(() => {
+//         cy.get('td').eq(cartPriceColumnIndex).should('contain.text', phonePrice);
+//       });
+//       cy.get('#tbodyid').then(($el) => {
+//       cy.log(`laptopName="[${laptopName}]" length=${laptopName.length}`);
+//       });
+//       cy.contains('#tbodyid tr', laptopName).within(() => {
+//         cy.get('td').eq(cartPriceColumnIndex).should('contain.text', laptopPrice);
+//       });
+//        cy.get('#totalp').should(($el) => {
+//         const expectedTotal = parseFloat(phonePrice) + parseFloat(laptopPrice);
+//         expect(Number($el.text())).to.eq(expectedTotal);
+//       });
+//     });
+//   });
+// });
